23c0472b4014115899a3c9a5de7a0d8ce8097585
Yun Yeo <sw.yunsuk@gmail.com>
2021-01-22 18:16:28 +0900

    fix to receive target asset

--- src/AutoStaker.ts
+++ src/AutoStaker.ts
@@ -1,4 +1,4 @@
-import { Mirror } from '@mirror-protocol/mirror.js';
+import { Mirror, AssetInfo } from '@mirror-protocol/mirror.js';
 import {
   MnemonicKey,
   LCDClient,
@@ -6,16 +6,18 @@ import {
   Wallet,
   isTxError,
   Int,
-  int
+  Coin
 } from '@terra-money/terra.js';
 
 const MNEMONIC = process.env.MNEMONIC;
 const MNEMONIC_INDEX = parseInt(process.env.MNEMONIC_INDEX || '0');
 const COIN_TYPE = parseInt(process.env.COIN_TYPE as string);
+const TARGET_ASSET = process.env.TARGET_ASSET || 'MIR';
 
 export default class AutoStaker {
   wallet: Wallet;
   mirror: Mirror;
+  lcd: LCDClient;
 
   constructor() {
     const key = new MnemonicKey({
@@ -37,6 +39,7 @@ export default class AutoStaker {
     });
 
     this.wallet = new Wallet(lcd, key);
+    this.lcd = lcd;
   }
 
   async execute(msgs: Array<MsgExecuteContract>) {
@@ -70,12 +73,21 @@ export default class AutoStaker {
   }
 
   async process() {
+    if (TARGET_ASSET === 'MIR') {
+      await this.processMIR();
+    } else {
+      await this.processNonMIR();
+    }
+  }
+
+  async processMIR() {
     const mirrorToken = this.mirror.assets['MIR'];
 
     const mirrorTokenAddr = mirrorToken.token.contractAddress as string;
     const poolInfo = await this.mirror.staking.getPoolInfo(mirrorTokenAddr);
     const rewardInfoResponse = await this.mirror.staking.getRewardInfo(
-      this.wallet.key.accAddress
+      this.wallet.key.accAddress,
+      mirrorTokenAddr
     );
 
     // if no rewards exists, skip procedure
@@ -147,6 +159,122 @@ export default class AutoStaker {
         mirrorTokenAddr,
         lpTokenBlanace.balance,
         mirrorToken.lpToken
+      )
+    ]);
+
+    console.log('Done');
+  }
+
+  async processNonMIR() {
+    const mirrorToken = this.mirror.assets['MIR'];
+    const assetToken = this.mirror.assets[TARGET_ASSET];
+
+    const assetTokenAddr = assetToken.token.contractAddress as string;
+    const poolInfo = await this.mirror.staking.getPoolInfo(assetTokenAddr);
+    const rewardInfoResponse = await this.mirror.staking.getRewardInfo(
+      this.wallet.key.accAddress,
+      assetTokenAddr
+    );
+
+    // if no rewards exists, skip procedure
+    if (poolInfo.reward_index == rewardInfoResponse.reward_infos[0].index) {
+      return;
+    }
+
+    console.log('Claim Rewards');
+    await this.execute([this.mirror.staking.withdraw()]);
+
+    const balanceResponse = await this.mirror.mirrorToken.getBalance();
+    const balance = new Int(balanceResponse.balance);
+
+    console.log('Swap rewards to UST');
+    await this.execute([
+      mirrorToken.pair.swap(
+        {
+          info: {
+            token: {
+              contract_addr: mirrorToken.token.contractAddress as string
+            }
+          },
+          amount: new Int(balance).toString()
+        },
+        {
+          offer_token: mirrorToken.token
+        }
+      )
+    ]);
+
+    console.log('Swap half UST to target asset');
+    const uusdBalanceResponse = await this.lcd.bank.balance(
+      this.wallet.key.accAddress
+    );
+
+    let pool = await assetToken.pair.getPool();
+    const uusdPool = new Int(pool.assets[0].amount);
+    const uusdBalance = (uusdBalanceResponse.get('uusd') as Coin).amount;
+
+    // let swap_amount = sqrt(pool*(pool + deposit)) - pool
+    const uusdSellAmount = new Int(
+      uusdPool.mul(uusdPool.plus(uusdBalance)).sqrt().minus(uusdPool)
+    );
+
+    await this.execute([
+      assetToken.pair.swap(
+        {
+          info: {
+            native_token: {
+              denom: 'uusd'
+            }
+          },
+          amount: uusdSellAmount.toString()
+        },
+        {}
+      )
+    ]);
+
+    const assetProvideAmount = new Int(
+      (await assetToken.token.getBalance()).balance
+    );
+
+    pool = await assetToken.pair.getPool();
+    const uusdProvideAmount = assetProvideAmount
+      .mul(pool.assets[0].amount)
+      .divToInt(pool.assets[1].amount);
+
+    console.log('Provide Liquidity');
+    await this.execute([
+      assetToken.token.increaseAllowance(
+        assetToken.pair.contractAddress as string,
+        new Int(assetProvideAmount).toString()
+      ),
+      assetToken.pair.provideLiquidity([
+        {
+          info: {
+            token: {
+              contract_addr: assetToken.token.contractAddress as string
+            }
+          },
+          amount: new Int(assetProvideAmount).toString()
+        },
+        {
+          info: {
+            native_token: {
+              denom: 'uusd'
+            }
+          },
+          amount: new Int(uusdProvideAmount).toString()
+        }
+      ])
+    ]);
+
+    const lpTokenBlanace = await assetToken.lpToken.getBalance();
+
+    console.log('Stake LP token');
+    await this.execute([
+      this.mirror.staking.bond(
+        assetTokenAddr,
+        lpTokenBlanace.balance,
+        assetToken.lpToken
       )
     ]);
 
